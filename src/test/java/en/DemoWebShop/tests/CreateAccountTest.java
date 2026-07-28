f70a3ac0f7367e35a26f8384363516678890abba
Nikolay <s3901058@gmail.com>
2026-07-28 17:39:45 +0200

    HW1

---
+++ src/test/java/en/DemoWebShop/tests/CreateAccountTest.java
@@ -0,0 +1,41 @@
+package en.DemoWebShop.tests;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.sql.Driver;
+
+public class CreateAccountTest extends TestBase {
+
+
+    @Test
+    public void newUserRegisterPositiveTest(){
+        click(By.cssSelector("[href='/register']"));
+        type(By.name("FirstName"), "John");
+        type(By.name("LastName"), "Doue");
+        type(By.name("Email"), "JohnTesatDoue@mail.de");
+        type(By.name("Password"), "123Q12a!");
+        type(By.name("ConfirmPassword"), "123Q12a!");
+        click(By.name("register-button"));
+        Assert.assertTrue(isElementPresent(By.xpath("//*[.='Log out']")));
+
+
+
+    }
+    @Test
+    public void ExistedUserRegisterNegativeTest(){
+        click(By.cssSelector("*[href='/register']"));
+        type(By.name("FirstName"), "John");
+        type(By.name("LastName"), "Doue");
+        type(By.name("Email"), "JohnTesatDoue@mail.de");
+        type(By.name("Password"), "123Q12a!");
+        type(By.name("ConfirmPassword"), "123Q12a!");
+        click(By.name("register-button"));
+        Assert.assertTrue(driver.findElement(By.cssSelector(".validation-summary-errors")).isDisplayed());
+
+    }
+
+}
