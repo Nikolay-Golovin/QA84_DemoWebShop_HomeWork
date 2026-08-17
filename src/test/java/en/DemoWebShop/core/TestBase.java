3777dc105e416d043a6052fb8ba1415251d115cc
Nikolay <s3901058@gmail.com>
2026-08-17 12:29:13 +0200

    HW2

--- src/test/java/en/DemoWebShop/core/TestBase.java
+++ src/test/java/en/DemoWebShop/core/TestBase.java
@@ -2,13 +2,19 @@ package en.DemoWebShop.core;
 
 import de.DemoWebShop.core.ApplicationManager;
 import org.openqa.selenium.remote.Browser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
 public class TestBase extends ApplicationManager {
 
 
     protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", Browser.CHROME.browserName()));
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     public TestBase(String browser) {
         super(browser);
@@ -23,6 +29,15 @@ public class TestBase extends ApplicationManager {
     @AfterSuite(enabled = false)
     public void tearDown()  {
         app.stop();
+    }
+
+    @BeforeMethod
+    public void startTest() {
+        logger.info("Start test");
+    }
+    @AfterMethod
+    public void stopTest(){
+        logger.info("Stop test");
     }
 
 }
